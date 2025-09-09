138e352f96fa58816bb30c84c63e60ca86044da8
StupidestJack <mamegoodbean@gmail.com>
2025-09-09 18:35:39 +0800

    Update 猜數字.py

--- 猜數字.py
+++ 猜數字.py
@@ -3,13 +3,14 @@ import random
 
 guess = random.randrange(1,100)
 while True:
-  a=input()
+  a=input("enter:")
   try:
+    a = int(a)
     if a < guess: print("太小")
     elif a > guess:print("太大")
     elif a == guess:
       print("對了")
       break
   except:
-    print(err)
+    print("err")
 input()
